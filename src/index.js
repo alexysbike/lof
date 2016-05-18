880c934638b74a407570aca8b9aeee086efbc5b1
airtoxin <airtoxin@icloud.com>
2016-05-19 08:35:41 +0900

    Fix?

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ const sum = (arr) => arr.reduce((a, v) => a + v);
 
 export const euclideanDistance = (p1, p2) => Math.sqrt(sum(zip(p1, p2).map((c1, c2) => Math.pow(c1 - c2), 2)))
 
-// k-Nearest Neighbors
-export const knn = (k, dataset, sourceData) => {
-    const kSubsetOfDataset = [[]];
-    return kSubsetOfDataset;
+// k-Nearest Neighbors (naive)
+export const knn = (k, dataset, sourceIndex) => {
+    const kNeighbors = dataset
+        .map((data, index) => [index, euclideanDistance(data, sourceData)])
+        .filter([index, distance] => index !== sourceIndex)
+        .sort(([index1, distance1], [index2, distance2]) => distance1 - distance2)
+        .slice(0, k)
+    return kNeighbors;
 };
 
 // Local Reachability Distance?
@@ -17,9 +21,9 @@ export const lrd = (kNeighbors, source) => kNeighbors.map((nn) => euclideanDista
 export const mlrd = (k, kNeighbors, source) => lrd(kNeighbors, source) / k;
 
 // Local Outlier Factor
-export const lof = (k, dataset, sourceData) => {
-    const mlrdFraction = knn(k, dataset, sourceData).map(kNeighbor => {
-        return mlrd(k, knn(k, dataset, sourceData), sourceData) / mlrd(k, knn(k, dataset, kNeighbor), kNeighbor);
+export const lof = (k, dataset, sourceIndex) => {
+    const mlrdFraction = knn(k, dataset, sourceIndex).map([kNeighborIndex, kNeighborDistance] => {
+        return mlrd(k, knn(k, dataset, sourceIndex), dataset[sourceIndex]) / mlrd(k, knn(k, dataset, kNeighborIndex), dataset[kNeighborIndex]);
     });
     return mlrdFraction / k;
 };
