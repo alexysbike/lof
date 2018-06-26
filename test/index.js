5c5638645d69f1787590a92f921f389631d8b0ac
Alexys Gonzalez <alexysbike@gmail.com>
2018-06-26 18:34:12 -0400

    WIP

--- test/index.js
+++ test/index.js
@@ -1,6 +1,7 @@
 import assert from "assert";
-import lof, {knn, kd, lrd} from "../src";
+import lofF, {knn, kd, lrd} from "../src";
 import euclideanDistance from "euclidean-distance";
+import fs from 'fs';
 
 // const nearlyEqual = (a, b) => Math.abs(a - b) < 0.00000001;
 //
@@ -66,21 +67,35 @@ import euclideanDistance from "euclidean-distance";
 
 describe('testing lof', () => {
   it('should return lof and be a number with k of 2', () => {
-    // const raw = [{"delay":0,"coordinates":[-33.451110392781,-70.812081545591]},{"delay":300,"coordinates":[-34.174431,-70.746095]},{"delay":300,"coordinates":[-34.182793,-70.770022]},{"delay":300,"coordinates":[-34.181455,-70.737295]},{"delay":300,"coordinates":[-34.173969,-70.742247]},{"delay":300,"coordinates":[-34.173756,-70.714255]},{"delay":300,"coordinates":[-34.181089,-70.741827]},{"delay":300,"coordinates":[-34.170693,-70.729641]},{"delay":300,"coordinates":[-34.183057,-70.73126]},{"delay":300,"coordinates":[-34.124135,-70.738878]},{"delay":300,"coordinates":[-34.178646,-70.736963]},{"delay":300,"coordinates":[-34.170082,-70.740729]},{"delay":300,"coordinates":[-34.173969,-70.742247]},{"delay":300,"coordinates":[-34.183057,-70.720274]},{"delay":300,"coordinates":[-34.164916,-70.7423]},{"delay":300,"coordinates":[18.08255,-63.052251]},{"delay":300,"coordinates":[-34.176755,-70.763049]},{"delay":300,"coordinates":[-34.128122,-70.740849]},{"delay":300,"coordinates":[-34.169424,-70.736753]},{"delay":300,"coordinates":[-34.168678,-70.747911]},{"delay":300,"coordinates":[-34.189427,-70.728419]},{"delay":300,"coordinates":[-34.155788,-70.720274]},{"delay":300,"coordinates":[-34.159325,-70.722356]},{"delay":300,"coordinates":[-34.162981,-70.69432]},{"delay":300,"coordinates":[-34.150788,-70.73415]},{"delay":300,"coordinates":[-33.42552,-70.566315]},{"delay":300,"coordinates":[-34.135728,-70.737914]},{"delay":300,"coordinates":[-34.183057,-70.742247]},{"delay":300,"coordinates":[-34.178777,-70.724169]},{"delay":300,"coordinates":[-34.183821,-70.748073]},{"delay":300,"coordinates":[-34.197805,-70.784669]}];
-    // const dataset = raw.map(item => item.coordinates);
+    const dataset = [{"delay":0,"coordinates":[-33.451110392781,-70.812081545591]},{"delay":300,"coordinates":[-34.174431,-70.746095]},{"delay":300,"coordinates":[-34.182793,-70.770022]},{"delay":300,"coordinates":[-34.181455,-70.737295]},{"delay":300,"coordinates":[-34.173969,-70.742247]},{"delay":300,"coordinates":[-34.173756,-70.714255]},{"delay":300,"coordinates":[-34.181089,-70.741827]},{"delay":300,"coordinates":[-34.170693,-70.729641]},{"delay":300,"coordinates":[-34.183057,-70.73126]},{"delay":300,"coordinates":[-34.124135,-70.738878]},{"delay":300,"coordinates":[-34.178646,-70.736963]},{"delay":300,"coordinates":[-34.170082,-70.740729]},{"delay":300,"coordinates":[-34.173969,-70.742247]},{"delay":300,"coordinates":[-34.183057,-70.720274]},{"delay":300,"coordinates":[-34.164916,-70.7423]},{"delay":300,"coordinates":[18.08255,-63.052251]},{"delay":300,"coordinates":[-34.176755,-70.763049]},{"delay":300,"coordinates":[-34.128122,-70.740849]},{"delay":300,"coordinates":[-34.169424,-70.736753]},{"delay":300,"coordinates":[-34.168678,-70.747911]},{"delay":300,"coordinates":[-34.189427,-70.728419]},{"delay":300,"coordinates":[-34.155788,-70.720274]},{"delay":300,"coordinates":[-34.159325,-70.722356]},{"delay":300,"coordinates":[-34.162981,-70.69432]},{"delay":300,"coordinates":[-34.150788,-70.73415]},{"delay":300,"coordinates":[-33.42552,-70.566315]},{"delay":300,"coordinates":[-34.135728,-70.737914]},{"delay":300,"coordinates":[-34.183057,-70.742247]},{"delay":300,"coordinates":[-34.178777,-70.724169]},{"delay":300,"coordinates":[-34.183821,-70.748073]},{"delay":300,"coordinates":[-34.197805,-70.784669]}];
+    const coordinates = dataset.map(item => item.coordinates);
+    fs.writeFileSync
+    // const dataset = [
+    //   [0, 0],
+    //   [0, 1],
+    //   [1, 1],
+    //   [3, 0],
+    // ];
 
-    const dataset = [
-      [0, 0],
-      [0, 1],
-      [1, 1],
-      [3, 0],
-    ];
-
-    dataset.forEach((item, index) => {
-      const result = lof(2, dataset, index);
-      console.log(result);
-      assert.equal(typeof result, 'number');
+    const results = dataset.map((item, index) => {
+      const lof = lofF(2, coordinates, index);
+      console.log(lof);
+      assert.equal(typeof lof, 'number');
+      return {
+        "type": "Feature",
+        "properties": {
+          lof,
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [...item.coordinates].reverse(),
+        }
+      };
     });
+
+    console.log(results);
+
+    fs.writeFileSync('mynewfile3.txt', JSON.stringify(results));
   });
 });
 
