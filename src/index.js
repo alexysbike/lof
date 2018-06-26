39f7d65c4709222c4668419d38eed44dd399bf43
Alexys Gonzalez <alexysbike@gmail.com>
2018-06-26 01:28:00 -0400

    Fixing another errors

--- src/index.js
+++ src/index.js
@@ -6,9 +6,13 @@ import {zip, sum} from "lodash";
 // k-Nearest Neighbors (naive)
 export const knn = (k, dataset, pIndex) => {
     return dataset
+        // calculate the distance from selected index to the others
         .map((data, index) => [index, euclideanDistance(data, dataset[pIndex])])
+        // filter the self point
         .filter(([index, distance]) => index !== pIndex)
+        // sort by nearest
         .sort(([index1, distance1], [index2, distance2]) => distance1 - distance2)
+        // get the K-neighbor
         .slice(0, k);
 };
 
@@ -16,38 +20,44 @@ export const knn = (k, dataset, pIndex) => {
 export const kd = (k, dataset, pIndex) => {
     const kNeighbors = knn(k, dataset, pIndex);
     const [index, distance] = kNeighbors.reduce((acc, dis) => acc[1] > dis[1] ? acc : dis);
+    // returning the bigger distance in the neighbors
     return distance;
 };
 
-// Reachability Distance of point p and point o
+// Reachability Distance of point p and point o (used by lrd only)
 export const rd = (k, dataset, pIndex, oIndex) => {
+    // getting the bigger between neighbors of current index and  distance between selected index and current index
     return Math.max(kd(k, dataset, oIndex), euclideanDistance(dataset[pIndex], dataset[oIndex]));
 };
 
+const sigmaRdCalc = (nearestArray, k, dataset, pIndex) => nearestArray
+// reachDistance: getting the max value between the Kth distance and the distance between selected index and current index for each item
+  .map(([oIndex, distance]) => rd(k, dataset, pIndex, oIndex))
+  // getting the sum of reachDistances
+  .reduce((d1, d2) => d1 + d2);
+
 // Local Reachability Density of point p
 export const lrd = (k, dataset, pIndex) => {
-    const sigmaRd = knn(k, dataset, pIndex)
-        .map(([oIndex, distance]) => rd(k, dataset, pIndex, oIndex))
-        .reduce((d1, d2) => d1 + d2);
-    return kd(k, dataset, pIndex) / sigmaRd;
+    // getting distances
+    const nearestArray = knn(k, dataset, pIndex);
+    const sigmaRd = sigmaRdCalc(nearestArray, k, dataset, pIndex);
+    // return kd(k, dataset, pIndex) / sigmaRd;
+    // was using the bigger distance in neighbors, instead I used length of pIndex neighbors (kNearestSetCount)
+    return nearestArray.length / sigmaRd;
 };
 
 // Local Outlier Factor
 export const lof = (k, dataset, pIndex) => {
-    console.log(
-      knn(k, dataset, pIndex)
-        .map(([oIndex]) => {
-          console.log(lrd(k, dataset, oIndex));
-          console.log(lrd(k, dataset, pIndex));
-          return lrd(k, dataset, oIndex) / lrd(k, dataset, pIndex)
-        })
-    );
-    const sigmaLrdFraction = knn(k, dataset, pIndex)
-        .map(([oIndex]) => lrd(k, dataset, oIndex) / lrd(k, dataset, pIndex))
-        .reduce((d1, d2) => d1 + d2);
-    console.log(sigmaLrdFraction);
-    console.log(kd(k, dataset, pIndex));
-    return sigmaLrdFraction / kd(k, dataset, pIndex);
+    const nearestArray = knn(k, dataset, pIndex);
+    // changing for other formula (https://medium.com/@hornbd96/local-outlier-factor-example-by-hand-b57cedb10bd1)
+    // const sigmaLrdFraction = nearestArray
+    //     .map(([oIndex]) => lrd(k, dataset, oIndex) / lrd(k, dataset, pIndex))
+    //     .reduce((d1, d2) => d1 + d2);
+    //
+    const sigmaLrdFraction = nearestArray
+      .map(([oIndex]) => lrd(k, dataset, oIndex))
+      .reduce((d1, d2) => d1 + d2);
+    return (sigmaLrdFraction * sigmaRdCalc(nearestArray, k, dataset, pIndex)) / nearestArray.length * nearestArray.length;
 };
 
 export default lof;
--- test/index.js
+++ test/index.js
@@ -1,5 +1,6 @@
 import assert from "assert";
 import lof, {knn, kd} from "../src";
+import euclideanDistance from "euclidean-distance";
 
 const nearlyEqual = (a, b) => Math.abs(a - b) < 0.00000001;
 
@@ -67,8 +68,10 @@ describe('testing lof', () => {
   const raw = [{"delay":0,"coordinates":[-33.451110392781,-70.812081545591]},{"delay":300,"coordinates":[-34.174431,-70.746095]},{"delay":300,"coordinates":[-34.182793,-70.770022]},{"delay":300,"coordinates":[-34.181455,-70.737295]},{"delay":300,"coordinates":[-34.173969,-70.742247]},{"delay":300,"coordinates":[-34.173756,-70.714255]},{"delay":300,"coordinates":[-34.181089,-70.741827]},{"delay":300,"coordinates":[-34.170693,-70.729641]},{"delay":300,"coordinates":[-34.183057,-70.73126]},{"delay":300,"coordinates":[-34.124135,-70.738878]},{"delay":300,"coordinates":[-34.178646,-70.736963]},{"delay":300,"coordinates":[-34.170082,-70.740729]},{"delay":300,"coordinates":[-34.173969,-70.742247]},{"delay":300,"coordinates":[-34.183057,-70.720274]},{"delay":300,"coordinates":[-34.164916,-70.7423]},{"delay":300,"coordinates":[18.08255,-63.052251]},{"delay":300,"coordinates":[-34.176755,-70.763049]},{"delay":300,"coordinates":[-34.128122,-70.740849]},{"delay":300,"coordinates":[-34.169424,-70.736753]},{"delay":300,"coordinates":[-34.168678,-70.747911]},{"delay":300,"coordinates":[-34.189427,-70.728419]},{"delay":300,"coordinates":[-34.155788,-70.720274]},{"delay":300,"coordinates":[-34.159325,-70.722356]},{"delay":300,"coordinates":[-34.162981,-70.69432]},{"delay":300,"coordinates":[-34.150788,-70.73415]},{"delay":300,"coordinates":[-33.42552,-70.566315]},{"delay":300,"coordinates":[-34.135728,-70.737914]},{"delay":300,"coordinates":[-34.183057,-70.742247]},{"delay":300,"coordinates":[-34.178777,-70.724169]},{"delay":300,"coordinates":[-34.183821,-70.748073]},{"delay":300,"coordinates":[-34.197805,-70.784669]}];
   const dataset = raw.map(item => item.coordinates);
 
-  const result = lof(2, dataset, 0);
-  console.log(result);
+  dataset.forEach((item, index) => {
+    const result = lof(2, dataset, index);
+    console.log(result);
+  });
 });
 
 // describe("lof function", () => {
